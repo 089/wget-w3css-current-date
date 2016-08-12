7ba48b32a9f6b15e83adccacf5a3ecfca4366408
089 <089@users.noreply.github.com>
2016-08-12 13:31:52 +0200

    Create wget-w3css-current-date.sh

---
+++ wget-w3css-current-date.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# Downloads the w3.css file and renames it using the current 
+# date. Additionally it generates a symbolic link which refers 
+# to the latest download.
+# 
+# Author: https://github.com/089
+# License: 
+
+# settings
+ULR=http://www.w3schools.com/lib/w3.css
+TODAY=`date +%Y-%m-%d`
+FILENAME="${TODAY}_w3.css"
+
+# download
+wget http://www.w3schools.com/lib/w3.css -O ${FILENAME}
+
+# symbolic link
+if [ -f ${FILENAME} ]
+then
+  rm ${FILENAME}
+fi
+
+ln -s ${FILENAME} latest-w3.css
